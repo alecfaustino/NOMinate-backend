552f963d6f670ea0e2f254e672dbf2c8c0346fd3
Alec Faustino <alecfaustino@outlook.com>
2025-08-18 15:31:28 -0600

    add response header for API layer

--- routes/spoonacular-api.js
+++ routes/spoonacular-api.js
@@ -1,56 +1,21 @@
 import express from "express";
 import axios from "axios";
 // API Documentation: https://spoonacular.com/food-api/docs
-const RECIPES_BASE_URL = "https://api.spoonacular.com/recipes";
+const RECIPES_BASE_URL = "https://api.apilayer.com/spoonacular/recipes";
 
 const router = express.Router();
-// fetch a single random recipe
-router.get("/random", async (req, res) => {
-  const apiKey = process.env.SPOON_API_KEY;
-  try {
-    const response = await axios.get(`${RECIPES_BASE_URL}/random`, {
-      params: {
-        apiKey,
-      },
-    });
-    const data = response.data;
-
-    res.status(200).json({ message: "Random recipe fetched: ", data });
-  } catch (error) {
-    console.error("Error fetching random recipe ", error);
-    res.status(500).json({ message: "Internal Server Error", error });
-  }
-});
-
-// fetch 10 random recipes
-
-router.get("/random10", async (req, res) => {
-  const apiKey = process.env.SPOON_API_KEY;
-  try {
-    const response = await axios.get(`${RECIPES_BASE_URL}/random`, {
-      params: {
-        apiKey,
-        number: 10,
-      },
-    });
-    const data = response.data;
-
-    res.status(200).json({ message: "10 recipes fetched: ", data });
-  } catch (error) {
-    console.error("Error fetching random recipe ", error);
-    res.status(500).json({ message: "Internal Server Error", error });
-  }
-});
 
 // fetch with filters -- main use now
 
 router.get("/search", async (req, res) => {
-  const apiKey = process.env.SPOON_API_KEY;
+  const apikey = process.env.SPOON_API_KEY;
   try {
     const response = await axios.get(`${RECIPES_BASE_URL}/complexSearch`, {
+      headers: {
+        apikey,
+      },
       params: {
-        apiKey,
-        number: 5,
+        number: 1,
         addRecipeInformation: true,
         addRecipeInstructions: true,
         addRecipeNutrition: true,
@@ -62,10 +27,16 @@ router.get("/search", async (req, res) => {
     });
     const data = response.data;
 
-    res.status(200).json({ message: "10 recipes fetched: ", data });
+    res.status(200).json({ message: "recipes fetched: ", data });
   } catch (error) {
-    // TODO: TEMP CODE - REPLACE WITH MEANINGFUL ERROR
-    console.error(error);
+    console.error(
+      "Spoonacular API error:",
+      error.response?.data || error.message
+    );
+    res.status(error.response?.status || 500).json({
+      message: "Failed to fetch recipes",
+      error: error.response?.data || error.message,
+    });
   }
 });
 
